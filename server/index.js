319099e966ec8aaaf1224191300635fd50c7d3ac
fullsnacker <fullsnacker@gmail.com>
2024-10-04 11:33:01 -0300

    Initial express server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+
+const itemsRoute = require("./routes/item");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+console.log(process.env.PORT);
+
+app.use("/api/items", itemsRoute);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
