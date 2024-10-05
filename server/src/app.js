6b59fb885cf950ae750cc96d136ab80a51739b08
fullsnacker <fullsnacker@gmail.com>
2024-10-05 16:49:47 -0300

    [SERVER] - added cors headers

--- server/src/app.js
+++ server/src/app.js
@@ -5,6 +5,21 @@ import itemsRoute from "./routes/items.js";
 
 const app = express();
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET,HEAD,OPTIONS,POST,PUT,DELETE"
+	);
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+	);
+	res.setHeader("Cache-Control", "no-cache");
+	next();
+});
+
 const PORT = process.env.PORT || 8080;
 
 app.use("/api/items", itemsRoute);
